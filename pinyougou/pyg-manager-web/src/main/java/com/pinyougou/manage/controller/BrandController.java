2f40cb0e465221fc3ce2c1f3b555b8637ff04152
liyi <757447147@qq.com>
2018-09-08 18:42:50 +0800

    使用angularJs分页查询品牌列表

--- pinyougou/pyg-manager-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pyg-manager-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -3,8 +3,10 @@ package com.pinyougou.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.vo.PageResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +16,29 @@ public class BrandController {
 
     @Reference
     private BrandService brandService;
+    /**
+     * 测试mybatis的分页助手pagehelper和通用mapper
+     */
+    @GetMapping("testPage")
+    public List<TbBrand> testPage(Integer page,Integer rows){
+        return brandService.testPage(page,rows);
+    }
 
+    /**
+     * 查询所有品牌信息
+     * @return
+     */
     @GetMapping("findAll")
     public List<TbBrand> quertAll(){
-        return brandService.queryAll();
+
+        /*return brandService.queryAll();*/
+        return brandService.findAll();
+    }
+
+    @GetMapping("findPage")
+    public PageResult findPage(@RequestParam(value = "page",defaultValue = "1") Integer page,
+                               @RequestParam(value = "rows",defaultValue = "5") Integer rows){
+
+        return brandService.findPage(page,rows);
     }
 }
--- pinyougou/pyg-sellergoods/pyg-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou/pyg-sellergoods/pyg-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,9 +1,22 @@
 package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.service.BaseService;
 
 import java.util.List;
 
-public interface BrandService {
+public interface BrandService extends BaseService<TbBrand> {
+    /**
+     * 查询所有品牌信息
+     * @return
+     */
     List<TbBrand> queryAll();
+
+    /**
+     * 测试分页助手和通用mapper
+     * @param page
+     * @param rows
+     * @return
+     */
+    List<TbBrand> testPage(Integer page, Integer rows);
 }
