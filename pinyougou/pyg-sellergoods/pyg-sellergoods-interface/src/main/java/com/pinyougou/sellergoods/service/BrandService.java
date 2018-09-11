8d74510da4b77c7946b95bd3470ed86616cd0145
liyi <757447147@qq.com>
2018-09-11 14:56:07 +0800

    .

--- pinyougou/pyg-sellergoods/pyg-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou/pyg-sellergoods/pyg-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -2,6 +2,7 @@ package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.service.BaseService;
+import com.pinyougou.vo.PageResult;
 
 import java.util.List;
 
@@ -19,4 +20,6 @@ public interface BrandService extends BaseService<TbBrand> {
      * @return
      */
     List<TbBrand> testPage(Integer page, Integer rows);
+
+    PageResult search(Integer page, Integer rows, TbBrand brand);
 }
